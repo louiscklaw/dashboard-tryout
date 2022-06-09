60c4bc72443a1c20f3d2600884877d171f897437
Louis Law <louiscklaw@users.noreply.github.com>
2022-06-09 18:22:08 +0800

    update docusaurus-dashboard,

--- docusaurus-dashboard/src/pages/weather.js
+++ docusaurus-dashboard/src/pages/weather.js
@@ -11,9 +11,27 @@ import {
 } from "@mui/material";
 import Link from "@docusaurus/Link";
 import { use100vh } from "react-div-100vh";
+import { useState } from "react";
+
+import { JsonToTable } from "react-json-to-table";
+const myJson = {
+  Student: { name: "Jack", email: "jack@xyz.com" },
+  "Student id": 888,
+  Sponsors: [
+    { name: "john", email: "john@@xyz.com" },
+    { name: "jane", email: "jane@@xyz.com" },
+  ],
+};
 
 export default function Home() {
   const height_100vh = use100vh();
+  const [hko_weather, setHkoWeather] = useState();
+
+  fetch(
+    "https://data.weather.gov.hk/weatherAPI/opendata/weather.php?dataType=rhrread&lang=en"
+  )
+    .then((res) => res.json())
+    .then((res_json) => setHkoWeather(res_json));
 
   return (
     <>
@@ -22,14 +40,9 @@ export default function Home() {
           <Typography variant="h5">Back</Typography>
         </Link>
       </Box>
-      <Grid container sx={{ height: height_100vh, overflowY: "hidden" }}>
+      <Grid container sx={{ height: height_100vh }}>
         <Grid item xs={4} sx={{ width: "100%", height: height_100vh / 2 }}>
-          <iframe
-            src="http://www.hkww.org/weather/chinese/index_main.html"
-            target="_parent"
-            height={height_100vh}
-            width="100%"
-          />
+          {hko_weather && <JsonToTable json={hko_weather} />}
         </Grid>
         <Grid item xs={4} sx={{ width: "100%", height: height_100vh / 2 }}>
           <iframe
