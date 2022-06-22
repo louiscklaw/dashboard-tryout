5e53bb7e2286fc8165011bea62b7d08bee17b4b2
Louis Law <louiscklaw@users.noreply.github.com>
2022-06-22 23:49:55 +0800

    update kmb-dashboard-tryout,

--- kmb-dashboard-tryout/src/components/BusStopPanelGrid/BusStopPanelGrid4.jsx
+++ kmb-dashboard-tryout/src/components/BusStopPanelGrid/BusStopPanelGrid4.jsx
@@ -48,37 +48,53 @@ export default function BusStopPanelGrid4() {
             <BusStopPanelContainer bus_stop_id={bus_stop_id4} />
           </Grid>
         </Grid>
-        <Container maxWidth="xl">
-          <Grid container>
-            <Grid item xs={4}></Grid>
-            <Grid
-              item
-              xs={4}
-              container
-              justifyContent="center"
-              alignItems="center"
-            >
-              <Stack
-                direction="row"
-                spacing={1}
-                justifyContent="flex-end"
-                alignItems={"flex-end"}
+
+        <Box
+          component="footer"
+          sx={{
+            py: 2,
+            px: 2,
+            mt: "auto",
+            backgroundColor: (theme) =>
+              theme.palette.mode === "light"
+                ? theme.palette.grey[200]
+                : theme.palette.grey[800],
+          }}
+        >
+          <Container maxWidth="xl">
+            <Grid container>
+              <Grid item xs={4}></Grid>
+              <Grid
+                item
+                xs={4}
+                container
+                justifyContent="center"
+                alignItems="center"
               >
-                <IconButtonWithLink
-                  link="https://github.com/louiscklaw/dashboard-tryout/tree/master/kmb-dashboard-tryout"
-                  icon={<FaGithub />}
-                />
-                <IconButtonWithLink
-                  link="https://www.linkedin.com/in/louiscklaw"
-                  icon={<FaLinkedin />}
-                />
-              </Stack>
+                <Stack
+                  direction="row"
+                  spacing={1}
+                  justifyContent="flex-end"
+                  alignItems={"flex-end"}
+                >
+                  <IconButtonWithLink
+                    link="https://github.com/louiscklaw/dashboard-tryout/tree/master/kmb-dashboard-tryout"
+                    icon={<FaGithub />}
+                  />
+                  <IconButtonWithLink
+                    link="https://www.linkedin.com/in/louiscklaw"
+                    icon={<FaLinkedin />}
+                  />
+                </Stack>
+              </Grid>
+              <Grid item xs={4} container justifyContent="flex-end">
+                <Typography variant="caption">
+                  last update: 2022-06-23
+                </Typography>
+              </Grid>
             </Grid>
-            <Grid item xs={4} container justifyContent="flex-end">
-              <Typography variant="caption">last update: 2022-06-23</Typography>
-            </Grid>
-          </Grid>
-        </Container>
+          </Container>
+        </Box>
       </Paper>
     </>
   );
