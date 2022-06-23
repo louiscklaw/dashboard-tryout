db5f7f348d36c6316eb3c97e4e4620f2a64d21df
Louis Law <louiscklaw@users.noreply.github.com>
2022-06-23 17:54:38 +0800

    update kmb-dashboard-tryout,

--- kmb-dashboard-tryout/src/components/BusStopPanelGrid/BusStopPanelGrid3.jsx
+++ kmb-dashboard-tryout/src/components/BusStopPanelGrid/BusStopPanelGrid3.jsx
@@ -2,12 +2,6 @@ import { Grid, Paper } from "@mui/material";
 import { useParams } from "react-router-dom";
 import BusStopPanelContainer from "../BusStopPanelContainer";
 
-const stop_eta_link = (stop_id) =>
-  `https://data.etabus.gov.hk/v1/transport/kmb/stop-eta/${stop_id}`;
-
-const stop_link = (stop_id) =>
-  `https://data.etabus.gov.hk/v1/transport/kmb/stop/${stop_id}`;
-
 export default function BusStopPanelGrid3() {
   let params = useParams();
   let { bus_stop_id1, bus_stop_id2, bus_stop_id3 } = params;
