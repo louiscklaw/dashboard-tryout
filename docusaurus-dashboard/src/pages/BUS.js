7ded43e1ad32d1cce6f2d808c247623f17754ecb
Louis Law <louiscklaw@users.noreply.github.com>
2022-06-23 18:22:40 +0800

    update docusaurus-dashboard,

--- docusaurus-dashboard/src/pages/BUS.js
+++ docusaurus-dashboard/src/pages/BUS.js
@@ -25,7 +25,7 @@ export default function Home() {
           </Link>
         </Box>
         <iframe
-          src="https://louiscklaw-bus-dashboard.web.app/bus_stops/41E81A921D79F375/C3201264CC8E5C5D/C1DAF9F8B637B776/6F58AF0D366EB8CA"
+          src="https://louiscklaw-bus-dashboard.web.app/bus_stops/41E81A921D79F375/C3201264CC8E5C5D/6CF34EF02CDBE26B/26305AF62FBEE2BC/1AEB39A1738104D3/7188D2451C5AF461"
           target="_parent"
           height={height_100vh}
           width="100%"
