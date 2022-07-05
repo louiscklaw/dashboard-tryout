92397ef0242b787b3e8b20111795fe78d72191e1
louiscklaw <louiscklaw@gmail.com>
2022-07-06 04:24:27 +0800

    update docusaurus-dashboard,

--- docusaurus-dashboard/docusaurus.config.js
+++ docusaurus-dashboard/docusaurus.config.js
@@ -139,6 +139,37 @@ const config = {
     },
     prism: { theme: lightCodeTheme, darkTheme: darkCodeTheme },
   },
+
+  plugins: [
+    [
+      "@docusaurus/plugin-pwa",
+      {
+        debug: true,
+        offlineModeActivationStrategies: [
+          "appInstalled",
+          "standalone",
+          "queryString",
+        ],
+        pwaHead: [
+          {
+            tagName: "link",
+            rel: "icon",
+            href: "/img/docusaurus.png",
+          },
+          {
+            tagName: "link",
+            rel: "manifest",
+            href: "/manifest.json", // your PWA manifest
+          },
+          {
+            tagName: "meta",
+            name: "theme-color",
+            content: "rgb(37, 194, 160)",
+          },
+        ],
+      },
+    ],
+  ],
 };
 
 module.exports = config;
