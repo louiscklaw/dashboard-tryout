faa3f7282b75fc40f9990794809b4eeca5f98e7e
Louis Law <louiscklaw@users.noreply.github.com>
2022-08-19 11:51:16 +0800

    update,

--- docusaurus-dashboard_wiki/src/components/ButtonWithStatus.jsx
+++ docusaurus-dashboard_wiki/src/components/ButtonWithStatus.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext'
-import { Grid, Stack, Typography } from '@mui/material'
+import { Box, Grid, Stack, Typography } from '@mui/material'
 import { use100vh } from 'react-div-100vh'
 import ButtonLink from './ButtonLink'
 // import ButtonLink from '../../../components/ButtonLink'
@@ -8,12 +8,21 @@ import ButtonLink from './ButtonLink'
 export default function ButtonWithStatus(props) {
   let { to, text, status_id } = props
 
+  const handleOnClick = () => {
+    window.open(`http://192.168.10.61:50102/dashboard/${status_id}`, '_blank')
+
+    // alert('helloworld')
+  }
+
   if (status_id) {
     return (
       <>
         <Stack spacing={2} direction="row" justifyContent="center" alignItems="center">
           <ButtonLink to={to} text={text} />
-          <img src={`http://192.168.10.61:50102/api/badge/${status_id}/status?upLabel=up&downLabel=down`} />
+
+          <Box onClick={handleOnClick}>
+            <img src={`http://192.168.10.61:50102/api/badge/${status_id}/status?upLabel=up&downLabel=down`} />
+          </Box>
         </Stack>
       </>
     )
