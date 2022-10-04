e088272c72b4f6b25023bda104e45ae46cf2783a
louiscklaw <louiscklaw@users.noreply.github.com>
2022-10-04 18:28:27 +0800

    Updates

---
+++ assets/js/ee4b6e83.b9cc48e1.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkwiki=self.webpackChunkwiki||[]).push([[4770],{610:t=>{t.exports=JSON.parse('{"permalink":"https://louiscklaw.github.io/dashboard-tryout/blog","page":1,"postsPerPage":10,"totalPages":1,"totalCount":1,"blogDescription":"7Wate \u7684\u4e2a\u4eba\u751f\u6d3b\u548c\u5de5\u4f5c\u8bb0\u5f55","blogTitle":"loislabs Blog"}')}}]);
--- zh-Hans/assets/js/2bf541da.41d4cc96.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkwiki=self.webpackChunkwiki||[]).push([[7564],{6786:e=>{e.exports=JSON.parse('{"permalink":"/zh-Hans/blog","page":1,"postsPerPage":10,"totalPages":1,"totalCount":1,"blogDescription":"7Wate \u7684\u4e2a\u4eba\u751f\u6d3b\u548c\u5de5\u4f5c\u8bb0\u5f55","blogTitle":"loislabs Blog"}')}}]);
