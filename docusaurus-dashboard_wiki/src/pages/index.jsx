cc7486e897972628ff70245e9c241d536f9a1302
louiscklaw <louiscklaw@users.noreply.github.com>
2022-12-20 01:14:22 +0800

    update wiki,

--- docusaurus-dashboard_wiki/src/pages/index.jsx
+++ docusaurus-dashboard_wiki/src/pages/index.jsx
@@ -2,7 +2,7 @@ import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
 import React from 'react';
 
-import { createTheme, Grid, TextField, ThemeProvider, Typography } from '@mui/material';
+import { Box, Button, createTheme, Grid, TextField, ThemeProvider, Typography } from '@mui/material';
 import ButtonWithStatus from '../components/ButtonWithStatus';
 
 import HighlightOffIcon from '@mui/icons-material/HighlightOff';
@@ -18,6 +18,7 @@ import outside_links from '../components/links/outside_links';
 import routine_check from '../components/links/routine_check';
 import tools from '../components/links/tools';
 import worksheets from '../components/links/worksheets';
+import { useColorMode, useThemeConfig } from '@docusaurus/theme-common';
 
 const carousell_configs = [
   profile,
@@ -37,79 +38,86 @@ function TestNewHome() {
   const handleFilterChange = e => setFilterValue(e.target.value);
 
   const handleClearSearchFilter = e => setFilterValue('');
+  const { colorMode, setColorMode } = useColorMode();
+  const theme = createTheme({
+    palette: { primary: { main: colorMode == 'light' ? '#2c3e50' : '#95a5a6' }, secondary: { main: '#95a5a6' } },
+  });
 
   return (
     <>
-      <Grid container spacing={2}>
-        <Grid item xs={12} m={4} container justifyContent="center" alignItems="center">
-          <Grid item xs={4}>
-            <TextField
-              fullWidth
-              id="outlined-basic"
-              label="search"
-              variant="filled"
-              color="primary"
-              onChange={handleFilterChange}
-              value={filter_value}
-              InputProps={{
-                endAdornment: (
+      <ThemeProvider theme={theme}>
+        <Grid container spacing={2}>
+          <Grid item xs={12} m={4} container justifyContent="center" alignItems="center">
+            <Grid item xs={4}>
+              <TextField
+                fullWidth
+                id="outlined-basic"
+                label="search"
+                variant="filled"
+                color="primary"
+                onChange={handleFilterChange}
+                value={filter_value}
+                sx={{
+                  backgroundColor: '#dddddd',
+                }}
+                InputProps={{
+                  endAdornment: (
+                    <>
+                      <InputAdornment position="end">
+                        <IconButton
+                          color="primary"
+                          aria-label="toggle password visibility"
+                          edge="end"
+                          onMouseDown={handleClearSearchFilter}
+                        >
+                          <HighlightOffIcon />
+                        </IconButton>
+                      </InputAdornment>
+                    </>
+                  ),
+                }}
+              />
+            </Grid>
+          </Grid>
+
+          <Grid item xs={12} container m={2}>
+            {carousell_configs.map(link_group => (
+              <>
+                <Grid item xs={12} m={2}>
+                  <Typography variant={'h5'}>{link_group.title}</Typography>
+                </Grid>
+
+                {link_group.links.map(link => (
                   <>
-                    <InputAdornment position="end">
-                      <IconButton
-                        aria-label="toggle password visibility"
-                        edge="end"
-                        onMouseDown={handleClearSearchFilter}
-                      >
-                        <HighlightOffIcon />
-                      </IconButton>
-                    </InputAdornment>
+                    <Grid item xs={3} xl={1.5}>
+                      <ButtonWithStatus
+                        to={link.to}
+                        text={link.text || ''}
+                        icon={link.icon || <WysiwygIcon />}
+                        subtitle={link.subtitle}
+                      />
+                    </Grid>
                   </>
-                ),
-              }}
-            />
+                ))}
+              </>
+            ))}
           </Grid>
         </Grid>
-
-        <Grid item xs={12} container m={2}>
-          {carousell_configs.map(link_group => (
-            <>
-              <Grid item xs={12} m={2}>
-                <Typography variant={'h5'}>{link_group.title}</Typography>
-              </Grid>
-
-              {link_group.links.map(link => (
-                <>
-                  <Grid item xs={3} xl={1.5}>
-                    <ButtonWithStatus
-                      to={link.to}
-                      text={link.text || ''}
-                      icon={link.icon || <WysiwygIcon />}
-                      subtitle={link.subtitle}
-                    />
-                  </Grid>
-                </>
-              ))}
-            </>
-          ))}
-        </Grid>
-      </Grid>
+      </ThemeProvider>
     </>
   );
 }
 
 export default function NewHome() {
   const { siteConfig } = useDocusaurusContext();
-  const theme = createTheme({ palette: { primary: { main: '#2c3e50' }, secondary: { main: '#7f8c8d' } } });
 
   return (
-    <ThemeProvider theme={theme}>
-      <Layout title={`Hello from ${siteConfig.title}`} description="Description will go into a meta tag in <head />">
-        {/* <HomepageHeader /> */}
-        <main>
-          <TestNewHome />
-          {/* <HomepageFeatures /> */}
-        </main>
-      </Layout>
-    </ThemeProvider>
+    <Layout title={`Hello from ${siteConfig.title}`} description="Description will go into a meta tag in <head />">
+      {/* <HomepageHeader /> */}
+      <main>
+        <TestNewHome />
+        {/* <HomepageFeatures /> */}
+      </main>
+    </Layout>
   );
 }
