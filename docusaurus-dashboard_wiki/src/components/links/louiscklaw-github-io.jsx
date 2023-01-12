6d0dc501b53b4189f355bf2b591224d98fc60f75
louiscklaw <louiscklaw@users.noreply.github.com>
2023-01-12 20:29:22 +0800

    update,

---
+++ docusaurus-dashboard_wiki/src/components/links/louiscklaw-github-io.jsx
@@ -0,0 +1,22 @@
+import GitHubIcon from '@mui/icons-material/GitHub';
+import PrintIcon from '@mui/icons-material/Print';
+import React from 'react';
+import { FaFileExcel } from 'react-icons/fa';
+import LinkIcon from '@mui/icons-material/Link';
+import SpeedIcon from '@mui/icons-material/Speed';
+
+export default {
+  title: 'outside links ',
+  links: [
+    {
+      to: 'https://pagespeed.web.dev/report?url=https%3A%2F%2Flouiscklaw.github.io%2Fpay',
+      text: 'google page speed',
+      subtitle: '',
+      status_id: 17,
+      icon_color: '#800000',
+      text_color: '#800000',
+      icon: <SpeedIcon size="2em" />,
+    },
+
+  ],
+};
