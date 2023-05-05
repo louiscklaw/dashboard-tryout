187661c01443ca4afd81546929d925d68dfaa524
Louis Law <louiscklaw@users.noreply.github.com>
2023-05-05 15:58:05 +0800

    update wiki,

--- docusaurus-dashboard_wiki/src/components/links/changedetect.jsx
+++ docusaurus-dashboard_wiki/src/components/links/changedetect.jsx
@@ -6,15 +6,6 @@ export default {
   title: 'change-detect',
   links: [
     {
-      to: 'http://carousell-changedetect.iamon99.com',
-      text: 'carousell',
-      subtitle: 'carousell-changedetect.iamon99.com',
-      status_id: 17,
-      icon_color: '#800000',
-      text_color: '#800000',
-      icon: <WysiwygIcon />,
-    },
-    {
       to: 'http://changedetect.iamon99.com',
       text: 'changedetect',
       subtitle: 'changedetect.iamon99.com',
@@ -24,34 +15,12 @@ export default {
       icon: <WysiwygIcon />,
     },
     {
-      to: 'http://private-changedetect.iamon99.com',
-      text: 'private',
-      subtitle: 'private-changedetect.iamon99.com',
-      status_id: 17,
-      icon_color: '#800000',
-      text_color: '#800000',
-    },
-    {
-      to: 'http://taobao-changedetect.iamon99.com',
-      text: 'taobao',
-      subtitle: 'taobao-changedetect.iamon99.com',
-      status_id: 17,
-      icon_color: '#800000',
-      text_color: '#800000',
-    },{
-      to: 'http://debug-changedetect.iamon99.com',
-      text: 'debug-changedetect',
-      subtitle: 'for debug use',
-      status_id: 17,
-      icon_color: '#800000',
-      text_color: '#800000',
-    },    {
       to: 'http://192.168.10.61:15999/metrics',
       text: 'browserless-metrics',
       subtitle: 'browserless metrics',
       status_id: 17,
       icon_color: '#800000',
       text_color: '#800000',
-    },    
+    },
   ],
 };
