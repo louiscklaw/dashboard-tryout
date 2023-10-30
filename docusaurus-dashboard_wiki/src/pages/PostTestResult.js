59e1e95670ae0ce8b56b3343217188457b99a44f
Louis Law <louiscklaw@users.noreply.github.com>
2023-10-30 10:27:41 +0800

    update,

--- docusaurus-dashboard_wiki/src/pages/PostTestResult.js
+++ docusaurus-dashboard_wiki/src/pages/PostTestResult.js
@@ -36,12 +36,14 @@ export default () => {
       <div style={{ fontSize: '0.75rem' }}>
         <div>post test result:</div>
         <div>{last_update}</div>
-        <div style={{ color: 'white', backgroundColor: vba_post_check_result == 'OK' ? 'green' : 'red' }}>
-          vba:{vba_post_check_result}
-        </div>
         <div style={{ color: 'white', backgroundColor: coding_post_check_result == 'OK' ? 'green' : 'red' }}>
           coding:{coding_post_check_result}
         </div>
+        
+        <div style={{ color: 'white', backgroundColor: vba_post_check_result == 'OK' ? 'green' : 'red' }}>
+          vba:{vba_post_check_result}
+        </div>
+    
         <div style={{ color: 'white', backgroundColor: programming_post_check_result == 'OK' ? 'green' : 'red' }}>
           programming:{programming_post_check_result}
         </div>
