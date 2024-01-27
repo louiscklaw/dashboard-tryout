1f33c10ae58bfd7425c01d9298427783e3754970
Louis Law <louiscklaw@users.noreply.github.com>
2024-01-28 04:52:09 +0800

    update,

--- docusaurus-dashboard_wiki/src/pages/DemoTestResult.jsx
+++ docusaurus-dashboard_wiki/src/pages/DemoTestResult.jsx
@@ -11,7 +11,18 @@ export default () => {
         <div>{last_update}</div>
 
         <div>
-          <img src="https://status.iamon99.com/api/badge/21/status?label=demo-react" />
+          <a
+            target="_blank"
+            rel="noopener noreferrer"
+            href="//demo-nextjs.iamon99.com/"
+            style={{
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'flex-end',
+            }}
+          >
+            <img src="//status.iamon99.com/api/badge/21/status?label=demo-nextjs.iamon99.com" />
+          </a>
         </div>
       </div>
     </>
