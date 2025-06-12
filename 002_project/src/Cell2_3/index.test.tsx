54e3533413a5c4bece8d86806fb86f8c6e4b3aab
louiscklaw <louiscklaw@users.noreply.github.com>
2025-06-12 19:38:46 +0800

    test: update test cases to reflect UI text changes and add new component tests

--- 002_project/src/Cell2_3/index.test.tsx
+++ 002_project/src/Cell2_3/index.test.tsx
@@ -1,9 +1,10 @@
 import { render, screen } from '@testing-library/react';
 import React from 'react';
+import { Cell2_3 } from '.';
 import App from './App';
 
 test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/All status table/i);
+  render(<Cell2_3 />);
+  const linkElement = screen.getByText(/lettersoup-demo/i);
   expect(linkElement).toBeInTheDocument();
 });
