12056b4d016db41131b4367b32b686f1d20f3280
louiscklaw <louiscklaw@users.noreply.github.com>
2025-06-12 15:55:56 +0800

    refactor: extract Cell1_3 component to separate file, update badge display logic

--- src/App.tsx
+++ src/App.tsx
@@ -12,6 +12,7 @@ import { ShowUptimeKumaBadge } from "./ShowUptimeKumaBadge";
 import { Cell1_6 } from "./Cell1_6";
 import { Cell1_4 } from "./Cell1_4";
 import { Cell1_5 } from "./Cell1_5";
+import { Cell1_3 } from "./Cell1_3";
 
 export function ShowHealthCheckBadge({
   imgsrc,
@@ -92,51 +93,6 @@ const Cell1_2 = (
         imgsrc={`${GetUptimeKumaBadgeLink(10)}?label=healthcheck.louislabs.com`}
         target_src={"//healthcheck.louislabs.com/"}
       />{" "}
-    </div>
-  </>
-);
-
-const Cell1_3 = (
-  <>
-    <div className="cell">
-      <div className="cell_title">cell1.3 (changedetect)</div>
-      <ShowUptimeKumaBadge
-        imgsrc={`${GetUptimeKumaBadgeLink(5)}?label=private`}
-        target_src={"http://192.168.10.21:5001/"}
-      />
-
-      <ShowUptimeKumaBadge
-        imgsrc={`${GetUptimeKumaBadgeLink(5)}?label=upsangel`}
-        target_src={"http://192.168.10.21:5008/"}
-      />
-      <ShowUptimeKumaBadge
-        imgsrc={`${GetUptimeKumaBadgeLink(41)}?label=carousell.louislabs.com`}
-        target_src={"http://192.168.10.21:5003/"}
-      />
-      <ShowUptimeKumaBadge
-        imgsrc={`${GetUptimeKumaBadgeLink(24)}?label=jobsdb`}
-        target_src={"http://192.168.10.21:5002/"}
-      />
-
-      <ShowUptimeKumaBadge
-        imgsrc={`${GetUptimeKumaBadgeLink(90)}?label=apple-refurbished`}
-        target_src={"http://192.168.10.21:5009/"}
-      />
-
-      <ShowUptimeKumaBadge
-        imgsrc={`${GetUptimeKumaBadgeLink(90)}?label=threads`}
-        target_src={"http://192.168.10.21:5010/"}
-      />
-
-      <ShowUptimeKumaBadge
-        imgsrc={`${GetUptimeKumaBadgeLink(90)}?label=debug`}
-        target_src={"http://192.168.10.21:5015/"}
-      />
-
-      <ShowUptimeKumaBadge
-        imgsrc={`${GetUptimeKumaBadgeLink(90)}?label=misc`}
-        target_src={"http://192.168.10.21:5016/"}
-      />
     </div>
   </>
 );
